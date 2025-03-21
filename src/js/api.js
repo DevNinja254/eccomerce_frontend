708ff2329626b34624e1feb05dce3531361650e6
Augustine Imbali <aga@devninja254.devninja254.io>
2025-03-21 14:42:47 +0300

    initial commit

--- src/js/api.js
+++ src/js/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1/', // Replace with your API base URL
+  baseURL: 'https://smooth-vast-thrush.ngrok-free.app/', // Replace with your API base URL
 });
 
 export default api;
